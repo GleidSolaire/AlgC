2186c239320bb62f7c833443736b821d18313dc7
glsiqueira <glsiqueira@IMAP-99900>
2018-11-30 11:59:29 -0200

    Exercicios

---
+++ Ex17/main.c
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: glsiqueira
+ *
+ * Created on 30 de Novembro de 2018, 10:25
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+/*
+ * 
+ */
+int
+main (int argc, char** argv)
+{
+                                             
+                                printf("  *        *      *  \n");
+                                  Sleep(1000);
+                                printf("           x     \n ");
+                                  Sleep(1000);
+                                printf("         xxx      \n");
+                                 Sleep(1000);
+                                printf("         xxxxx     \n");
+                                 Sleep(1000);
+                                printf("        xxxxxxx     \n");
+                                 Sleep(1000);
+                                printf("       xxxxxxxxx     \n");
+                                 Sleep(1000);
+                                printf("      xxxxxxxxxxx     \n");
+                                 Sleep(1000);
+                                printf("     xxxxxxxxxxxxx     \n");
+                                 Sleep(1000);
+                                printf("    xxxxxxxxxxxxxxx     \n");
+                                 Sleep(1000);
+                                printf("   xxxxxxxxxxxxxxxxx     \n");
+                                 Sleep(1000);
+                                printf("  xxxxxxxxxxxxxxxxxxx     \n"); 
+                                 Sleep(1000);
+                                printf(" xxxxxxxxxxxxxxxxxxxxx    \n");
+                                 Sleep(1000);
+                                printf("          xxx              \n");                        
+                                printf("          xxx               \n");
+                                printf("         xxxxx               \n");
+                                
+                                printf("\n");
+                                printf(" ************************ \n")
+                                printf(" *** MERRY CHRISTIMAS *** \n")
+                                printf(" ************************
+ 
+                               
+                                
+             
+                                
+                                
+    
+  return (EXIT_SUCCESS);
+}
+
