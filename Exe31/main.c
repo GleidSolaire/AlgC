48f67f25d74168ba880d8a2e1d760c58d82836e8
glsiqueira <glsiqueira@IMAP-99900>
2018-12-04 11:45:31 -0200

    Exercicios feito na IDE do netbeans

---
+++ Exe31/main.c
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: glsiqueira
+ *
+ * Created on 4 de Dezembro de 2018, 08:27
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int
+main (int argc, char** argv)
+{
+
+  
+int n, contador, i;
+
+	for (i = 0; i < 5; i++) {
+		printf("Informe um numero inteiro %i: ", i + 1);
+		scanf("%d", &n);
+		if (a < 0) {
+			contador++;
+		}
+	}
+	printf("\nForam informados %i numeros negativos\n\n", contador);
+         
+       
+      
+  
+  return (EXIT_SUCCESS);
+}
+
